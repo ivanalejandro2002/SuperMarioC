170a2971c986fd6629657c2c469c096830f8bade
Ivan_Olguin <ivanalejandro2002@gmail.com>
2023-12-02 12:36:47 -0600

    inicio de imagenes

--- main.c
+++ main.c
@@ -1,24 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <windows.h>
 #include <conio.h>
+#include <allegro.h>
 #define YSCREEN 16
 #define XSCREEN 22
-#define ESCALA 1
+#define ESCALA 8
 #define ESC         27
 #define ARRIBA      72
 #define ABAJO       80
 #define IZQUIERDA   75
 #define DERECHA     77
-#define SALTOMIN    62
-#define SALTOMAY    122
-#define FACTORMHOR  0.01
+#define SALTOMAY    62
+#define SALTOMIN    122
+#define CORROMAY    61
+#define CORROMIN    121
+#define FACTORMHOR  0.04
 #define MAXVELH     0.5
 #define FAUTOM      0.1
-#define ESPERA      100000000
+#define ESPERA      700
 #define ACCY        0.1
 #define AUMENTOVERT 0.8
 #define MAXVELV     0.9
+#define TECLA_ARRIBA    0
+#define TECLA_ABAJO     2
+#define TECLA_DERECHA   1
+#define TECLA_IZQUIERDA 3
+#define SALTO           4
+#define CORRER          5
 struct coord{
     float x;
     float y;
@@ -31,17 +39,19 @@ int xcamara,ycamara;
 char **mundo;
 char buffer[YSCREEN+2][XSCREEN+2];
 char minimundo[YSCREEN+2][XSCREEN+2];
-void color(int X){
+char oprimidas[6];
+char oprimo;
+/*void color(int X){
    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),X);      ///cambia el color del texto
-}
-void gotoxy(int x, int y){
+}*/
+/*void gotoxy(int x, int y){
     HANDLE hcon;
     COORD dwpos;
     dwpos.X=x;
     dwpos.Y=y;
     hcon=GetStdHandle(STD_OUTPUT_HANDLE);               ///cambia la poscición del cursor
     SetConsoleCursorPosition(hcon,dwpos);
-}
+}*/
 char teclear(){
     char tecla;
   if(kbhit()){
@@ -79,7 +89,18 @@ void inicializa(){
 }
 void retardo(){
     int x = ESPERA;
-    while(x>0)x--;
+    while(x>0){
+        if(kbhit())
+        oprimo = teclear();
+        else oprimo = 0;
+        if(oprimo==ARRIBA)oprimidas[TECLA_ARRIBA]=1;
+        if(oprimo==DERECHA)oprimidas[TECLA_DERECHA]=1;
+        if(oprimo==IZQUIERDA)oprimidas[TECLA_IZQUIERDA]=1;
+        if(oprimo==ABAJO)oprimidas[TECLA_ABAJO]=1;
+        if(oprimo==SALTOMAY || oprimo == SALTOMIN)oprimidas[SALTO]=1;
+        if(oprimo==CORROMAY || oprimo==CORROMIN)oprimidas[CORRER]=1;
+        x--;
+    }
 }
 void render(){
     int i,j;
@@ -103,9 +124,16 @@ void render(){
     }
 }
 void juega(){
+    int cont;
     while(1){
-        gotoxy(0,0);
+        cont = 0;
+        //gotoxy(0,0);
+        system("cls");
         retardo();
+        for(int i =0;i<6;i++){
+            if(oprimidas[i])cont++;
+            oprimidas[i]=0;
+        }
         render();
         char mov = teclear();
         if(mov){
@@ -122,14 +150,16 @@ void juega(){
                 mario.vy = -AUMENTOVERT;
             }
         }else{
-            if(mario.vh<0){
-                mario.vh+=FAUTOM;
-                if(mario.vh>0)mario.vh=0;
-            }else if(mario.vh>0){
-                mario.vh-=FAUTOM;
-                if(mario.vh<0)mario.vh=0;
-            }else{
-                mario.estado='Q';
+            if(mario.vy==0){
+                if(mario.vh<0){
+                    mario.vh+=FAUTOM;
+                    if(mario.vh>0)mario.vh=0;
+                }else if(mario.vh>0){
+                    mario.vh-=FAUTOM;
+                    if(mario.vh<0)mario.vh=0;
+                }else{
+                    mario.estado='Q';
+                }
             }
         }
         if(mario.x+mario.vh<=0){
@@ -149,27 +179,41 @@ void juega(){
         mario.y+=mario.vy;
 
         mario.x+=mario.vh;
-        gotoxy(0,YSCREEN+1);
+        //gotoxy(0,YSCREEN+1);
         printf("                     ");
         printf("%f",mario.vh);
-        gotoxy(0,YSCREEN+2);
+        //gotoxy(0,YSCREEN+2);
         printf("                     ");
         printf("%f",mario.x);
-        gotoxy(0,YSCREEN+3);
+        //gotoxy(0,YSCREEN+3);
         printf("                     ");
-        gotoxy(0,YSCREEN+3);
+        //gotoxy(0,YSCREEN+3);
         printf("%f",mario.vy);
-        gotoxy(0,YSCREEN+4);
+        //gotoxy(0,YSCREEN+4);
         printf("                     ");
-        gotoxy(0,YSCREEN+4);
+        //gotoxy(0,YSCREEN+4);
         printf("%f",mario.y);
-
-
+        //gotoxy(0,YSCREEN+5);
+        printf("                     ");
+        //gotoxy(0,YSCREEN+5);
+        printf("%d",cont);
     }
 }
 int main()
 {
+    allegro_init();
+    install_keyboard();
+    set_color_depth(32);
+    set_gfx_mode(GFX_AUTODETECT_WINDOWED,640,480, 0, 0);
+        if (install_sound(DIGI_AUTODETECT, MIDI_AUTODETECT, NULL) != 0) {
+       allegro_message("Error: inicializando sistema de sonido\n%s\n", allegro_error);
+       return 1;
+    }
+    BITMAP *buffer = create_bitmap(640,480);
+    clear_to_color(buffer, 0x33FF5B);
+
     inicializa();
     juega();
     return 0;
 }
+END_OF_MAIN();
